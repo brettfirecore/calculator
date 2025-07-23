e62be7cd3cf525b5b9acf9146025fcf17133a625
brettfirecore <klavian333@gmail.com>
2025-07-23 16:44:13 +1200

    Updated .gitignore and calculator files

--- calculator.go
+++ calculator.go
@@ -1,12 +1,13 @@
-// Package calculator does simple calculations.
 package calculator
 
-// Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
 	return a + b
 }
 
-// Subtract takes two numbers a and b, and returns the result of subtracting b from a.
+func Multiply(a, b float64) float64 {
+	return a * b
+}
+
 func Subtract(a, b float64) float64 {
 	return a - b
 }
