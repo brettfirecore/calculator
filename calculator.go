ec7adac9612c6cc6329f9909e042c7ade2f2e99c
brettfirecore <klavian333@gmail.com>
2025-09-19 14:06:40 +1200

    Update calculator.go altered line 13 swapped b and a around, ran tests PASSED

--- calculator.go
+++ calculator.go
@@ -3,6 +3,12 @@ package calculator
 
 // Add takes two numbers and returns the result of adding
 // them together.
-func Add(a,  b float64) float64 {
+func Add(a, b float64) float64 {
 	return a + b
+}
+
+// Subtract takes two number a and b, and
+// returns the result of subtracting b from a.
+func Subtract(a, b float64) float64 {
+	return a - b // swap b and a around so its 2 from 4 not 4 from 2 
 }
--- calculator_test.go
+++ calculator_test.go
@@ -7,12 +7,23 @@ import (
 )
 
 func TestAdd(t *testing.T) {
-	t.Parallel()
+	t.Parallel() // run test in parallel with other tests
 
 	got := calculator.Add(2, 3) // this is float64
 	want := 5.0                 // must be float64 too
 
-	if got != want {
+	if got != want { // if got is not equal to want then run t.Fatalf error
 		t.Fatalf("got %f want %f", got, want)
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+
+	var want float64 = 2
+	got := calculator.Subtract(4, 2) // 4->(b) minus from 2->(a) is -2 throws an error
+									 // refer calculator.go line 13
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
 	}
 }
