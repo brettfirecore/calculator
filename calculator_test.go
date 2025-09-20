d8d0161c5d8274b20b790f73763ae2f32af33094
brettfirecore <klavian333@gmail.com>
2025-09-20 14:05:48 +1200

    added TestMultiply to calculator_test.go ran all tests

--- calculator_test.go
+++ calculator_test.go
@@ -8,12 +8,20 @@ import (
 
 func TestAdd(t *testing.T) {
 	t.Parallel() // run test in parallel with other tests
-
-	got := calculator.Add(2, 3) // this is float64
-	want := 5.0                 // must be float64 too
-
-	if got != want { // if got is not equal to want then run t.Fatalf error
-		t.Fatalf("got %f want %f", got, want)
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 1, b: 1, want: 2},
+		{a: 5, b: 0, want: 5},
+	}
+	for _, tc := range testCases {
+		got := calculator.Add(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Add(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
 	}
 }
 
